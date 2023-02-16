e1e53744b028e7d25ab8407c27f541016732e4a5
Shoshana <shoshanadorfman@gmail.com>
2023-02-15 19:49:06 -0500

    Wild Wild Life answered

--- Module1/total_recall/d2_total_recall.js
+++ Module1/total_recall/d2_total_recall.js
@@ -20,22 +20,38 @@ for (let i = 0; i <= 200; i++) {
 // Fizz Buzz
 ////////////////////////////////
 
-for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0){
-       console.log("FizzBuzz"); 
-    } else if (i % 5 === 0) {
-       console.log("Buzz"); 
-    } else if (i % 3 === 0) {
-        console.log("Fizz");
-    } else {
-        console.log(i);
-    }    
-}
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0){
+//        console.log("FizzBuzz"); 
+//     } else if (i % 5 === 0) {
+//        console.log("Buzz"); 
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz");
+//     } else {
+//         console.log(i);
+//     }    
+// }
 
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
 
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+plantee[2]++;
+wolfy.splice(3, 1, "Gotham City");
+dart.push("Hawkins")
+wolfy.shift()
+wolfy.unshift("Gameboy")
+
+// console.log(plantee);
+// console.log(wolfy);
+// console.log(dart);
+// console.log(wolfy);
 
 ////////////////////////////////
 // Yell at the Ninja Turtles
