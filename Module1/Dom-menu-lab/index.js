c2df73643be320d204a731fd040f28b318faf544
Shoshana <shoshanadorfman@gmail.com>
2023-02-22 13:59:36 -0500

    added event listeners

--- Module1/Dom-menu-lab/index.js
+++ Module1/Dom-menu-lab/index.js
@@ -1,10 +1,4 @@
-// Menu data structure
-var menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
+
 //Task 1
 const mainEl = document.querySelector('main');
 
@@ -21,20 +15,28 @@ topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
 topMenuEl.classList.add('flex-around');
 
 //Task 3
-//iterate over menuLinks array 
+// Menu data structure
+const menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+//iterate over menuLinks array - for Loop
 for (i = 0; i < menuLinks.length; i++) {
     // Create an <a>element.
    const a = document.createElement('a')
-    //  On the new element, add an href attribute 
-    // with its value set to the href property of the "link" object.  
+    //  On the new element, add an href attribute with its value set to the href property of the "link" object.  
    a.setAttribute('href', menuLinks[i].href);
    console.log(a);
-   
    //Set the new element's content to the value of the text property of the "link" object.
    a.textContent = menuLinks[i].text;
-    // Append the new element to the topMenuElelement.
+    // Append the new element to the topMenuEl element.
     topMenuEl.appendChild(a);   
 }
+
+//Can also use For Each Loop
+
 
 console.log(mainEl);
 console.dir(mainEl);
